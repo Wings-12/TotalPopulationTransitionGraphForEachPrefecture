fe2a93093c2087949da1e455ce7534c18964a871
Tsubasa.Oumi <ohmiwing@gmail.com>
2022-07-14 18:54:09 +0900

    人口3桁区切り&小数点3桁区切り表示&表示可能行&表をチェックボックスチェック時に表示に修正

--- tpoputransgraphforeachpref/src/components/PopulationCompositionTable.jsx
+++ tpoputransgraphforeachpref/src/components/PopulationCompositionTable.jsx
@@ -21,12 +21,12 @@ const PopulationCompositionTable = () => {
   let totalPopulation = 0;
 
   return (
-    <table border={"1"}>
-      <tbody>
-        <tr>
-          <th>{sortInDescendingOrder()}西暦</th>
-          {populationCompositionObj.result &&
-            populationCompositionObj.result.data.map((data4Types, i) => {
+    populationCompositionObj.result && (
+      <table border={"1"}>
+        <tbody>
+          <tr>
+            <th>{sortInDescendingOrder()}西暦</th>
+            {populationCompositionObj.result.data.map((data4Types, i) => {
               return (
                 <Fragment key={i}>
                   <th>{data4Types.label + population}</th>
@@ -38,59 +38,63 @@ const PopulationCompositionTable = () => {
                 </Fragment>
               );
             })}
-        </tr>
+          </tr>
 
-        {populationCompositionObj.result &&
-          populationCompositionObj.result.data[0].data.map((value, index) => {
+          {populationCompositionObj.result.data[0].data.map((value, index) => {
             return (
               <tr key={index}>
                 <td>
-                  {populationCompositionObj.result &&
-                    populationCompositionObj.result.data[0].data[index].year}
+                  {populationCompositionObj.result.data[0].data[index].year}
                 </td>
 
-                {populationCompositionObj.result &&
-                  populationCompositionObj.result.data.map((data4Types, i) => {
-                    return (
-                      <Fragment key={i}>
-                        <td>{data4Types.data[index].value}</td>
-                        <td>
-                          {(() => {
-                            if (data4Types.label === "総人口") {
-                              totalPopulation = data4Types.data[index].value;
-                              return index !== 17
-                                ? Math.round(
-                                    (totalPopulation /
+                {populationCompositionObj.result.data.map((data4Types, i) => {
+                  return (
+                    <Fragment key={i}>
+                      <td>{data4Types.data[index].value.toLocaleString()}</td>
+                      <td>
+                        {(() => {
+                          let result = 0;
+                          const lastIndex =
+                            populationCompositionObj.result.data[0].data
+                              .length - 1;
+                          if (data4Types.label === "総人口") {
+                            totalPopulation = data4Types.data[index].value;
+                            return index !== lastIndex
+                              ? (() => {
+                                  result =
+                                    ((totalPopulation /
                                       data4Types.data[index + 1].value -
                                       1) *
                                       100 *
-                                      100
-                                  ) /
-                                    100 +
-                                    "%"
-                                : "-";
-                            } else {
-                              return (
-                                Math.round(
-                                  (data4Types.data[index].value /
+                                      100) /
+                                    100;
+                                  return result.toFixed(2);
+                                })() + "%"
+                              : "-";
+                          } else {
+                            return (
+                              (() => {
+                                result =
+                                  ((data4Types.data[index].value /
                                     totalPopulation) *
                                     100 *
-                                    100
-                                ) /
-                                  100 +
-                                "%"
-                              );
-                            }
-                          })()}
-                        </td>
-                      </Fragment>
-                    );
-                  })}
+                                    100) /
+                                  100;
+                                return result.toFixed(2);
+                              })() + "%"
+                            );
+                          }
+                        })()}
+                      </td>
+                    </Fragment>
+                  );
+                })}
               </tr>
             );
           })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    )
   );
 };
 
